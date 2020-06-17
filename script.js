6f1d9098345bedc3cc4528071721ac9c1760df80
HardCoreCorder <gudduag23@gmail.com>
2020-06-17 20:36:48 +0530

    c

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const c1 = document.querySelector('.c1');
 const c2 = document.querySelector('.c2');
 const c3 = document.querySelector('.c3');
 const c4 = document.querySelector('.c4');
-
+/*
 window.addEventListener('resize',e=>{
     var screenWidth = window.innerWidth;
     if(screenWidth<768){
@@ -13,6 +13,18 @@ window.addEventListener('resize',e=>{
     }else if(screenWidth>768){
         reveal.classList.add('d-none');
         hide.classList.remove('d-none');
+    }
+});*/
+let widthMatch = window.matchMedia("(max-width: 768px)");
+
+widthMatch.addEventListener('change', e=> {
+    if (e.matches) {
+      reveal.classList.remove('d-none');
+      hide.classList.add('d-none');
+    }
+    else {
+      reveal.classList.add('d-none');
+      hide.classList.remove('d-none');
     }
 });
 window.addEventListener('scroll',e=>{
