b311594d5590414333c1c34751d10f6e4973f252
HardCoreCorder <gudduag23@gmail.com>
2020-06-17 21:05:04 +0530

    e

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-const reveal = document.querySelector('.reveal');
-const hide = document.querySelector('.hide');
 const c1 = document.querySelector('.c1');
 const c2 = document.querySelector('.c2');
 const c3 = document.querySelector('.c3');
 const c4 = document.querySelector('.c4');
 
 window.addEventListener('scroll',e=>{
-  c1.classList.add('animated','fadeInUp','delay-1s');
-  c2.classList.add('animated','fadeInUp','delay-2s');
-  c3.classList.add('animated','fadeInUp','delay-2s');
-  c4.classList.add('animated','fadeInUp','delay-3s');
+  c1.classList.add('animated','fadeInDown','delay-1s');
+  c2.classList.add('animated','fadeInLeft','delay-1s');
+  c3.classList.add('animated','fadeInRight','delay-1s');
+  c4.classList.add('animated','fadeInUp','delay-1s');
 });
 
