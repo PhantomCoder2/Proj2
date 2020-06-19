602a79a78cd753956931e8b8258eebd23853d4a9
HardCoreCorder <gudduag23@gmail.com>
2020-06-19 13:14:37 +0530

    home done

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const c4 = document.querySelector('.c4');
 
 window.addEventListener('scroll',e=>{
   c1.classList.add('animated','fadeInDown','delay-1s');
-  c2.classList.add('animated','fadeInLeft','delay-1s');
-  c3.classList.add('animated','fadeInRight','delay-1s');
-  c4.classList.add('animated','fadeInUp','delay-1s');
+  c2.classList.add('animated','fadeInDown','delay-2s');
+  c3.classList.add('animated','fadeInUp','delay-3s');
+  c4.classList.add('animated','fadeInUp','delay-4s');
 });
 
